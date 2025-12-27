fa7af3b325721380781b1d8e7768314e11f9913a
Carlos Clemente <carlos.clemente.abc@gmail.com>
2025-12-27 23:45:17 +0100

    Version v.0.1.0.0 	- Primera version 	- Añadido formulario principal con barra de titulo, de herramientas y panel para el visor 	- Funcionalidad implementada para mover el formulario 	- Pendiente de implementar funcionalidad para redimensionar ventana y botones de navegacion de PDF

---
+++ VisorPDF/frmVisorPDF.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace VisorPDF
+{
+    partial class frmVisorPDF
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVisorPDF));
+            pnlTitulo = new Panel();
+            btnFijar = new Button();
+            pnlSuperior = new Panel();
+            label1 = new Label();
+            btnMaximizar = new Button();
+            ctrlVentana = new ImageList(components);
+            btnMinimizar = new Button();
+            btnCerrar = new Button();
+            pnlHerramientas = new Panel();
+            btnPrimero = new Button();
+            ctrlPDF = new ImageList(components);
+            btnRetroceso = new Button();
+            btnAvance = new Button();
+            btnUltimo = new Button();
+            pnlVisor = new Panel();
+            pnlIzquierdo = new Panel();
+            pnlDerecho = new Panel();
+            pnlInferior = new Panel();
+            pnlTitulo.SuspendLayout();
+            pnlHerramientas.SuspendLayout();
+            pnlVisor.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlTitulo
+            // 
+            pnlTitulo.BackColor = Color.SandyBrown;
+            pnlTitulo.Controls.Add(btnFijar);
+            pnlTitulo.Controls.Add(pnlSuperior);
+            pnlTitulo.Controls.Add(label1);
+            pnlTitulo.Controls.Add(btnMaximizar);
+            pnlTitulo.Controls.Add(btnMinimizar);
+            pnlTitulo.Controls.Add(btnCerrar);
+            pnlTitulo.Dock = DockStyle.Top;
+            pnlTitulo.Location = new Point(0, 0);
+            pnlTitulo.Name = "pnlTitulo";
+            pnlTitulo.Size = new Size(800, 40);
+            pnlTitulo.TabIndex = 0;
+            pnlTitulo.MouseDown += pnlTitulo_MouseDown;
+            // 
+            // btnFijar
+            // 
+            btnFijar.Anchor = AnchorStyles.Top;
+            btnFijar.BackColor = Color.Transparent;
+            btnFijar.FlatAppearance.BorderSize = 0;
+            btnFijar.FlatAppearance.MouseOverBackColor = Color.PeachPuff;
+            btnFijar.FlatStyle = FlatStyle.Flat;
+            btnFijar.Font = new Font("Calibri", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnFijar.Image = Properties.Resources.Activo;
+            btnFijar.ImageAlign = ContentAlignment.MiddleRight;
+            btnFijar.Location = new Point(335, 2);
+            btnFijar.Name = "btnFijar";
+            btnFijar.Size = new Size(130, 36);
+            btnFijar.TabIndex = 4;
+            btnFijar.Text = "Fijar ventana";
+            btnFijar.TextAlign = ContentAlignment.MiddleLeft;
+            btnFijar.UseVisualStyleBackColor = false;
+            btnFijar.Click += btnFijar_Click;
+            // 
+            // pnlSuperior
+            // 
+            pnlSuperior.BackColor = Color.LightGray;
+            pnlSuperior.Cursor = Cursors.SizeNS;
+            pnlSuperior.Dock = DockStyle.Top;
+            pnlSuperior.Location = new Point(0, 0);
+            pnlSuperior.Name = "pnlSuperior";
+            pnlSuperior.Size = new Size(800, 3);
+            pnlSuperior.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(5, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Visor PDF";
+            // 
+            // btnMaximizar
+            // 
+            btnMaximizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMaximizar.BackColor = Color.Transparent;
+            btnMaximizar.FlatAppearance.BorderSize = 0;
+            btnMaximizar.FlatAppearance.MouseOverBackColor = Color.PeachPuff;
+            btnMaximizar.FlatStyle = FlatStyle.Flat;
+            btnMaximizar.ImageIndex = 1;
+            btnMaximizar.ImageList = ctrlVentana;
+            btnMaximizar.Location = new Point(709, 2);
+            btnMaximizar.Name = "btnMaximizar";
+            btnMaximizar.Size = new Size(36, 36);
+            btnMaximizar.TabIndex = 2;
+            btnMaximizar.UseVisualStyleBackColor = false;
+            btnMaximizar.Click += btnMaximizar_Click;
+            // 
+            // ctrlVentana
+            // 
+            ctrlVentana.ColorDepth = ColorDepth.Depth32Bit;
+            ctrlVentana.ImageStream = (ImageListStreamer)resources.GetObject("ctrlVentana.ImageStream");
+            ctrlVentana.TransparentColor = Color.Transparent;
+            ctrlVentana.Images.SetKeyName(0, "Cerrar.png");
+            ctrlVentana.Images.SetKeyName(1, "Maximizar.png");
+            ctrlVentana.Images.SetKeyName(2, "Minimizar.png");
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimizar.BackColor = Color.Transparent;
+            btnMinimizar.FlatAppearance.BorderSize = 0;
+            btnMinimizar.FlatAppearance.MouseOverBackColor = Color.PeachPuff;
+            btnMinimizar.FlatStyle = FlatStyle.Flat;
+            btnMinimizar.ImageIndex = 2;
+            btnMinimizar.ImageList = ctrlVentana;
+            btnMinimizar.Location = new Point(654, 2);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(36, 36);
+            btnMinimizar.TabIndex = 1;
+            btnMinimizar.UseVisualStyleBackColor = false;
+            btnMinimizar.Click += btnMinimizar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCerrar.BackColor = Color.Transparent;
+            btnCerrar.FlatAppearance.BorderSize = 0;
+            btnCerrar.FlatAppearance.MouseOverBackColor = Color.PeachPuff;
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.ImageIndex = 0;
+            btnCerrar.ImageList = ctrlVentana;
+            btnCerrar.Location = new Point(761, 2);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(36, 36);
+            btnCerrar.TabIndex = 0;
+            btnCerrar.UseVisualStyleBackColor = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // pnlHerramientas
+            // 
+            pnlHerramientas.BackColor = Color.Moccasin;
+            pnlHerramientas.Controls.Add(btnPrimero);
+            pnlHerramientas.Controls.Add(btnRetroceso);
+            pnlHerramientas.Controls.Add(btnAvance);
+            pnlHerramientas.Controls.Add(btnUltimo);
+            pnlHerramientas.Dock = DockStyle.Top;
+            pnlHerramientas.ForeColor = Color.Black;
+            pnlHerramientas.Location = new Point(0, 40);
+            pnlHerramientas.Name = "pnlHerramientas";
+            pnlHerramientas.Size = new Size(800, 40);
+            pnlHerramientas.TabIndex = 1;
+            // 
+            // btnPrimero
+            // 
+            btnPrimero.BackColor = Color.Transparent;
+            btnPrimero.FlatAppearance.BorderSize = 0;
+            btnPrimero.FlatAppearance.MouseOverBackColor = Color.SeaShell;
+            btnPrimero.FlatStyle = FlatStyle.Flat;
+            btnPrimero.ImageIndex = 1;
+            btnPrimero.ImageList = ctrlPDF;
+            btnPrimero.Location = new Point(5, 1);
+            btnPrimero.Name = "btnPrimero";
+            btnPrimero.Size = new Size(36, 36);
+            btnPrimero.TabIndex = 8;
+            btnPrimero.UseVisualStyleBackColor = false;
+            // 
+            // ctrlPDF
+            // 
+            ctrlPDF.ColorDepth = ColorDepth.Depth32Bit;
+            ctrlPDF.ImageStream = (ImageListStreamer)resources.GetObject("ctrlPDF.ImageStream");
+            ctrlPDF.TransparentColor = Color.Transparent;
+            ctrlPDF.Images.SetKeyName(0, "Avance.png");
+            ctrlPDF.Images.SetKeyName(1, "Primero.png");
+            ctrlPDF.Images.SetKeyName(2, "Retroceso.png");
+            ctrlPDF.Images.SetKeyName(3, "Ultimo.png");
+            // 
+            // btnRetroceso
+            // 
+            btnRetroceso.BackColor = Color.Transparent;
+            btnRetroceso.FlatAppearance.BorderSize = 0;
+            btnRetroceso.FlatAppearance.MouseOverBackColor = Color.SeaShell;
+            btnRetroceso.FlatStyle = FlatStyle.Flat;
+            btnRetroceso.ImageIndex = 2;
+            btnRetroceso.ImageList = ctrlPDF;
+            btnRetroceso.Location = new Point(56, 1);
+            btnRetroceso.Name = "btnRetroceso";
+            btnRetroceso.Size = new Size(36, 36);
+            btnRetroceso.TabIndex = 7;
+            btnRetroceso.UseVisualStyleBackColor = false;
+            // 
+            // btnAvance
+            // 
+            btnAvance.BackColor = Color.Transparent;
+            btnAvance.FlatAppearance.BorderSize = 0;
+            btnAvance.FlatAppearance.MouseOverBackColor = Color.SeaShell;
+            btnAvance.FlatStyle = FlatStyle.Flat;
+            btnAvance.ImageIndex = 0;
+            btnAvance.ImageList = ctrlPDF;
+            btnAvance.Location = new Point(107, 1);
+            btnAvance.Name = "btnAvance";
+            btnAvance.Size = new Size(36, 36);
+            btnAvance.TabIndex = 6;
+            btnAvance.UseVisualStyleBackColor = false;
+            // 
+            // btnUltimo
+            // 
+            btnUltimo.BackColor = Color.Transparent;
+            btnUltimo.FlatAppearance.BorderSize = 0;
+            btnUltimo.FlatAppearance.MouseOverBackColor = Color.SeaShell;
+            btnUltimo.FlatStyle = FlatStyle.Flat;
+            btnUltimo.ImageIndex = 3;
+            btnUltimo.ImageList = ctrlPDF;
+            btnUltimo.Location = new Point(158, 1);
+            btnUltimo.Name = "btnUltimo";
+            btnUltimo.Size = new Size(36, 36);
+            btnUltimo.TabIndex = 5;
+            btnUltimo.UseVisualStyleBackColor = false;
+            // 
+            // pnlVisor
+            // 
+            pnlVisor.BackColor = Color.Black;
+            pnlVisor.Controls.Add(pnlIzquierdo);
+            pnlVisor.Controls.Add(pnlDerecho);
+            pnlVisor.Controls.Add(pnlInferior);
+            pnlVisor.Dock = DockStyle.Fill;
+            pnlVisor.Location = new Point(0, 80);
+            pnlVisor.Name = "pnlVisor";
+            pnlVisor.Size = new Size(800, 520);
+            pnlVisor.TabIndex = 2;
+            // 
+            // pnlIzquierdo
+            // 
+            pnlIzquierdo.BackColor = Color.LightGray;
+            pnlIzquierdo.Cursor = Cursors.SizeWE;
+            pnlIzquierdo.Dock = DockStyle.Left;
+            pnlIzquierdo.Location = new Point(0, 0);
+            pnlIzquierdo.Name = "pnlIzquierdo";
+            pnlIzquierdo.Size = new Size(3, 517);
+            pnlIzquierdo.TabIndex = 2;
+            // 
+            // pnlDerecho
+            // 
+            pnlDerecho.BackColor = Color.LightGray;
+            pnlDerecho.Cursor = Cursors.SizeWE;
+            pnlDerecho.Location = new Point(797, 0);
+            pnlDerecho.Name = "pnlDerecho";
+            pnlDerecho.Size = new Size(3, 557);
+            pnlDerecho.TabIndex = 1;
+            // 
+            // pnlInferior
+            // 
+            pnlInferior.BackColor = Color.LightGray;
+            pnlInferior.Cursor = Cursors.SizeNS;
+            pnlInferior.Dock = DockStyle.Bottom;
+            pnlInferior.Location = new Point(0, 517);
+            pnlInferior.Name = "pnlInferior";
+            pnlInferior.Size = new Size(800, 3);
+            pnlInferior.TabIndex = 0;
+            // 
+            // frmVisorPDF
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 600);
+            Controls.Add(pnlVisor);
+            Controls.Add(pnlHerramientas);
+            Controls.Add(pnlTitulo);
+            DoubleBuffered = true;
+            Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            MinimumSize = new Size(800, 600);
+            Name = "frmVisorPDF";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            pnlTitulo.ResumeLayout(false);
+            pnlTitulo.PerformLayout();
+            pnlHerramientas.ResumeLayout(false);
+            pnlVisor.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlTitulo;
+        private Panel pnlHerramientas;
+        private Button btnCerrar;
+        private Panel pnlVisor;
+        private Button btnMaximizar;
+        private Button btnMinimizar;
+        private Label label1;
+        private Button btnFijar;
+        private Button button3;
+        private ImageList imageList1;
+        private Button btnPrimero;
+        private Button btnAvance;
+        private Button btnUltimo;
+        private Button btnRetroceso;
+        private ImageList ctrlVentana;
+        private Panel panel3;
+        private Panel pnlSuperior;
+        private Panel pnlDerecho;
+        private Panel pnlInferior;
+        private ImageList ctrlPDF;
+        private Panel pnlIzquierdo;
+    }
+}
---
+++ VisorPDF/frmVisorPDF.cs
@@ -0,0 +1,80 @@
+using System.Runtime.InteropServices;
+
+
+namespace VisorPDF
+{
+    public partial class frmVisorPDF : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(
+            IntPtr hWnd,
+            int msg,
+            IntPtr wParam,
+            IntPtr lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        public frmVisorPDF()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFijar_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+            this.btnFijar.Image = this.TopMost ? Properties.Resources.Activo : Properties.Resources.Inactivo;
+        }
+
+        private void pnlTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button != MouseButtons.Left)
+                return;
+
+            // Doble clic para maximizar/restaurar
+            if(e.Clicks == 2)
+            {
+                if(this.WindowState == FormWindowState.Maximized)
+                {
+                    this.WindowState = FormWindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = FormWindowState.Maximized;
+                }
+
+                return;
+            }
+
+            // Clic simple para mover la 
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, (IntPtr)HTCAPTION, IntPtr.Zero);
+        }
+    }
+}
